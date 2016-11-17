1d55c76624c7c2eff7d6b8ce698824b518b1a72e
Jacqueline <jamjacqueline@gmail.com>
2016-11-17 00:14:29 -0300

    ultimos cambios

---
+++ js/main.js
@@ -0,0 +1,185 @@
+
+
+function validateForm(){
+
+	function validarNombre(){
+		var caractInv = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
+		//var nombre =document.getElementById("name").value;
+		var nombre= $("#name").val();
+		if(nombre.length==0){
+
+			//var divContenedor=document.getElementsByClassName("name-container")[0];
+			//var elemento= document.createElement("span");
+			//var nodoTexto=document.createTextNode("Este campo no debe quedar vacío");
+			//elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			//divContenedor.appendChild(elemento);
+			var mensajeError="Este campo no debe quedar vacío";
+			var span=$("#name").parent().append("<span>" + mensajeError+ "</span>");
+
+			return false;
+		}
+		if(!caractInv.test(nombre)){
+			//var nombre= $("#name").val();
+			var mensajeError="Debe escribir caracteres validos";
+			var span=$("#name").parent().append("<span>" + mensajeError+ "</span>");
+			//var divContenedor=document.getElementsByClassName("name-container")[0];
+			//var elemento= document.createElement("span");
+			//var nodoTexto=document.createTextNode("Debe escribir caracteres validos");
+			//elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			//divContenedor.appendChild(elemento);
+
+
+			return false;
+		} 
+	}
+	validarNombre();
+
+
+	function validarApellido(){
+		var caractInv = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
+		//var apellido=document.getElementById("lastname").value;
+		var apellido= $("#lastname").val();
+		if(apellido.length==0){
+			var mensajeError="Este campo no debe quedar vacío";
+			var span=$("#lastname").parent().append("<span>" + mensajeError+ "</span>");
+			//var divContenedor=document.getElementsByClassName("lastname-container")[0];
+			//var elemento= document.createElement("span");
+			//var nodoTexto=document.createTextNode("Este campo no debe quedar vacío");
+			//elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			//divContenedor.appendChild(elemento);
+
+			return false;
+		}
+		if(!caractInv.test(apellido)){
+			var mensajeError="Debe escribir caracteres validos";
+			var span=$("#lastname").parent().append("<span>" + mensajeError+ "</span>");
+			/*var divContenedor=document.getElementsByClassName("lastname-container")[0];
+			var elemento= document.createElement("span");
+			var nodoTexto=document.createTextNode("Debe escribir caracteres validos");
+			elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			divContenedor.appendChild(elemento);*/
+
+			return false;
+		}
+
+	}
+	validarApellido();
+
+	function validaMayuscula (){
+		var nombre= $("#name").val();
+		var apellido= $("#lastname").val();
+
+		if(nombre.charAt(0) !== nombre.charAt(0).toUpperCase()){
+
+			var divContenedor=document.getElementsByClassName("name-container")[0];
+			var elemento= document.createElement("span");
+			var nodoTexto=document.createTextNode("La primera letra debe ser mayúscula");
+			elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			divContenedor.appendChild(elemento);
+
+			return false;
+		}
+
+		if(apellido.charAt(0) !== apellido.charAt(0).toUpperCase()){
+
+			var divContenedor=document.getElementsByClassName("lastname-container")[0];
+			var elemento= document.createElement("span");
+			var nodoTexto=document.createTextNode("La primera letra debe ser mayúscula");
+			elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			divContenedor.appendChild(elemento);
+
+			return false;
+		}
+
+	}
+	validaMayuscula ();
+
+
+	function validarCorreo(){
+		var valideMail= /\w+@\w+\.+[a-z]/; 
+		var correo=document.getElementById("input-email").value;
+		if(correo.length==0){
+			var divContenedor=document.getElementsByClassName("email-container")[0];
+			var elemento= document.createElement("span");
+			var nodoTexto=document.createTextNode("Este campo no debe quedar vacío");
+			elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			divContenedor.appendChild(elemento);
+
+			return false;
+		}
+		if (!valideMail.test(correo)){
+
+			var divContenedor=document.getElementsByClassName("email-container")[0];
+			var elemento= document.createElement("span");
+			var nodoTexto=document.createTextNode("Escriba un formato válido");
+			elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			divContenedor.appendChild(elemento);
+
+			return false;
+		}
+	}
+	validarCorreo();
+
+
+	function validarContrasena(){
+		var contrasena=document.getElementById("input-password").value;
+		if(contrasena.length==0){
+			var divContenedor=document.getElementsByClassName("form-group")[0];
+			var elemento= document.createElement("span");
+			var nodoTexto=document.createTextNode("Este campo no debe quedar vacío");
+			elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			divContenedor.appendChild(elemento);
+
+			return false;
+		}if(contrasena==="123456" ||  contrasena==="098754" || contrasena==="password" && contrasena.length<6){
+
+			var divContenedor=document.getElementsByClassName("form-group")[0];
+			var elemento= document.createElement("span");
+			var nodoTexto=document.createTextNode("Contraseña muy débil, debe cambiarla por una segura");
+			elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			divContenedor.appendChild(elemento);
+
+			return false;
+		}
+
+	}
+	validarContrasena();
+
+
+	function validarSeleccion(){
+		var seleccion=document.getElementsByTagName("select")[0].value;
+		if(seleccion ==0){
+			var divContenedor=document.getElementsByClassName("form-group")[1];
+			var elemento= document.createElement("span");
+			var nodoTexto=document.createTextNode("Debe elegir una bicicleta");
+			elemento.appendChild(nodoTexto);//elemento es padre de nodoTexto;
+			divContenedor.appendChild(elemento);
+
+			return false;
+		}
+
+	}
+	validarSeleccion();
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
